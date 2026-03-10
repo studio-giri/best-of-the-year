9818f8f299a97911c7dabe2c4f98ad99077fad67
Paulin Trognon <contact@paulintrognon.fr>
2026-03-10 22:11:02 +0100

    feat(rankings): complete schema, seed & migration

--- apps/backend/src/db/schema.ts
+++ apps/backend/src/db/schema.ts
@@ -1,6 +1,30 @@
 import { sql } from "drizzle-orm";
-import { pgTable, uuid } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const rankingsTable = pgTable("rankings", {
 	id: uuid("id").primaryKey().default(sql`uuidv7()`),
+
+	author: varchar({
+		length: 30,
+	}).notNull(),
+
+	createdAt: timestamp("created_at", {
+		mode: "date",
+		precision: 3,
+	})
+		.notNull()
+		.defaultNow(),
+
+	updatedAt: timestamp("updated_at", {
+		mode: "date",
+		precision: 3,
+	})
+		.notNull()
+		.defaultNow()
+		.$onUpdate(() => new Date()),
+
+	deletedAt: timestamp("deleted_at", {
+		mode: "date",
+		precision: 3,
+	}),
 });
---
+++ apps/backend/src/db/scripts/seed-values.ts
@@ -0,0 +1,5 @@
+import type { rankingsTable } from "../schema";
+
+export const ranking1: typeof rankingsTable.$inferInsert = {
+	author: "My Username",
+};
